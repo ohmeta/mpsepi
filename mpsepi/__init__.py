6e478ae288850f29c98f5935d2c892ddb1bf0f32
alienzj <alienchuj@gmail.com>
2023-01-05 17:30:12 +0800

    add scaffold scripts

---
+++ mpsepi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from mpsepi.configer import mpseconfig
+from mpsepi.configer import parse_yaml
+from mpsepi.configer import update_config
+from mpsepi.configer import custom_help_formatter
+
+from mpsepi.__about__ import __version__, __author__
+
+name = "mpsepi"
