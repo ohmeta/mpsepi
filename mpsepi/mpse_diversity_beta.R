f403f7335da67ebbd0d863c22e6686bfe8de4fb4
alienzj <alienchuj@gmail.com>
2023-01-05 14:40:39 +0800

    refine diversity_beta

--- mpsepi/mpse_diversity_beta.R
+++ mpsepi/mpse_diversity_beta.R
@@ -3,7 +3,7 @@
 'mpse diversity beta script
 
 Usage:
-  mpse_diversity_beta.R <distmethod> <mpse> <group> <dist_tsv> <dist_samples_plot_prefix> <dist_groups_plot_prefix> <pcoa_plot_prefix> <image>
+  mpse_diversity_beta.R <method> <distmethod> <mpse> <group> <dist_tsv> <dist_samples_plot_prefix> <dist_groups_plot_prefix> <pcoa_plot_prefix> <clust_plot_prefix> <image>
   mpse_import.R (-h | --help)
   mpse_import.R --version
 
@@ -17,9 +17,16 @@ Options:
 library(magrittr)
 library(ggplot2)
 library(patchwork)
+library(ggtree)
+library(ggtreeExtra)
 
 
 args <- docopt::docopt(doc, version = 'mpse diversity beta v0.1')
+
+s_prefix <- args$dist_samples_plot_prefix
+g_prefix <- args$dist_groups_plot_prefix
+p_prefix <- args$pcoa_plot_prefix
+c_prefix <- args$clust_plot_prefix
 
 readRDS(args$mpse)
 
@@ -43,40 +50,41 @@ mpse_dist <-
 
 readr::write_tsv(mpse_dist, args$dist_tsv)
 
-
+# samples distance
 p1 <- mpse %>% MicrobiotaProcess::mp_plot_dist(.distmethod = args$distmethod, .group = args$group)
 
+
+# groups distance
 p2 <- mpse %>% MicrobiotaProcess::mp_plot_dist(.distmethod = args$distmethod, .group = args$group, group.test = TRUE, textsize = 2)
 
-pcoa_p1 <- mpse %>%
-        mp_plot_ord(
-          .ord = pcoa, 
-          .group = time, 
-          .color = time, 
-          .size = 1.2,
-          .alpha = 1,
-          ellipse = TRUE,
-          show.legend = FALSE
-        )
 
-pcoa_p2 <- mpse %>% 
-        mp_plot_ord(
-          .ord = pcoa, 
-          .group = time, 
-          .color = time, 
-          .size = Observe, 
-          .alpha = Shannon,
-          ellipse = TRUE,
-          show.legend = FALSE
-        )
+# pcoa
+pcoa_p1 <- mpse %>%
+  MicrobiotaProcess::mp_plot_ord(
+    .ord = pcoa, 
+    .group = args$group, 
+    .color = args$group, 
+    .size = 1.2,
+    .alpha = 1,
+    ellipse = TRUE,
+    show.legend = FALSE
+)
+
+pcoa_p2 <- mpse %>%
+  MicrobiotaProcess::mp_plot_ord(
+    .ord = pcoa, 
+    .group = args$group, 
+    .color = args$group, 
+    .size = Observe, 
+    .alpha = Shannon,
+    ellipse = TRUE,
+    show.legend = FALSE
+)
 
 pcoa_p <- pcoa_p1 + pcoa_p2
 
-s_prefix <- args$dist_samples_plot_prefix
-g_prefix <- args$dist_groups_plot_prefix
-p_prefix <- args$pcoa_plot_prefix
 
-
+# save plot
 ggsave(stringr::str_c(s_prefix, ".pdf"), p1)
 ggsave(stringr::str_c(s_prefix, ".svg"), p1)
 ggsave(stringr::str_c(s_prefix, ".png"), p1)
@@ -88,6 +96,73 @@ ggsave(stringr::str_c(g_prefix, ".png"), p2)
 ggsave(stringr::str_c(p_prefix, ".pdf"), pcoa_p)
 ggsave(stringr::str_c(p_prefix, ".svg"), pcoa_p)
 ggsave(stringr::str_c(p_prefix, ".png"), pcoa_p)
+
+
+# clust
+mpse %<>%
+  MicrobiotaProcess::mp_cal_clust(
+    .abundance = hellinger, 
+    distmethod = args$distmethod,
+    hclustmethod = "average",
+    action = "add"
+)
+
+samples_clust <- mpse %>% MicrobiotaProcess::mp_extract_internal_attr(name='SampleClust')
+
+f <- ggtree(sample_clust) + 
+  geom_tippoint(aes(color = args$group)) +
+  geom_tiplab(as_ylab = TRUE) +
+  ggplot2::scale_x_continuous(expand = c(0, 0.01))
+
+phyla_tb <- mpse %>% MicrobiotaProcess::mp_extract_abundance(taxa.class = Phylum, topn = 30)
+
+if (args$method %in% c("dada2", "qiime2")) {
+
+  phyla_tb %<>% tidyr::unnest(cols = RareAbundanceBySample) %>% dplyr::rename(Phyla = "label")
+  f <- f +
+    geom_fruit(
+      data = phyla_tb,
+      geom = geom_col,
+      mapping = aes(x = RelRareAbundanceBySample, 
+                    y = Sample, 
+                    fill = Phyla),
+      orientation = "y",
+      #offset = 0.4,
+      pwidth = 3, 
+      axis.params = list(axis = "x", 
+                         title = "The relative abundance of phyla (%)",
+                         title.size = 4,
+                         text.size = 2, 
+                         vjust = 1),
+      grid.params = list()
+    )
+
+} else if (args$method == "metaphlan") {
+
+  phyla_tb %<>% tidyr::unnest(cols = AbundanceBySample) %>% dplyr::rename(Phyla = "label")
+
+  f <- f +
+    geom_fruit(
+      data = phyla_tb,
+      geom = geom_col,
+      mapping = aes(x = RelAbundanceBySample, 
+                    y = Sample, 
+                    fill = Phyla),
+      orientation = "y",
+      #offset = 0.4,
+      pwidth = 3, 
+      axis.params = list(axis = "x", 
+                         title = "The relative abundance of phyla (%)",
+                         title.size = 4,
+                         text.size = 2, 
+                         vjust = 1),
+      grid.params = list()
+    )
+}
+
+ggsave(stringr::str_c(c_prefix, ".pdf"), f)
+ggsave(stringr::str_c(c_prefix, ".svg"), f)
+ggsave(stringr::str_c(c_prefix, ".png"), f)
 
 
 save.image(args$image)
--- mpsepi/rules/mpse_diversity_beta.smk
+++ mpsepi/rules/mpse_diversity_beta.smk
@@ -12,18 +12,23 @@ rule mpse_diversity_beta:
         pcoa_plot = expand(os.path.join(
             config["output"]["diversity_beta"], "pcoa_plot/pcoa.{format}",
             format=["pdf", "svg", "png"])),
+        clust_plot = expand(os.path.join(
+            config["output"]["diversity_beta"], "clust_plot/clust.{format}",
+            format=["pdf", "svg", "png"])),
         image = os.path.join(config["output"]["diversity_beta"], "image/diversity_beta.RData")
     params:
         group = config["params"]["group"],
+        method = config["params"]["import_from"],
         distmethod = config["params"]["diversity_beta"]["distmethod"],
         dist_samples_plot_prefix = config["output"]["diversity_beta"], "dist_plot/dist_samples",
         dist_groups_plot_prefix = config["output"]["diversity_beta"], "dist_plot/dist_groups",
-        pcoa_plot_prefix = config["output"]["diversity_beta"], "pcoa_plot/pcoa"
+        pcoa_plot_prefix = config["output"]["diversity_beta"], "pcoa_plot/pcoa",
+        clust_plot_prefix = config["output"]["diversity_beta"], "clust_plot/clust"
     conda:
         config["envs"]["mpse"]
     shell:
         '''
-        Rscript ../mpse_diversity_beta.R \
+        Rscript ../mpse_diversity_beta.R {params.method} \
         {params.distmethod} \
         {input} \
         {params.group} \
@@ -31,5 +36,6 @@ rule mpse_diversity_beta:
         {params.dist_samples_plot_prefix} \
         {params.dist_groups_plot_prefix} \
         {params.pcoa_plot_prefix} \
+        {params.clust_plot_prefix} \
         {output.image}
         '''
