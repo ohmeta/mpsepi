2cb13a6b0c85667510e98122cf4e51c60cc8762b
alienzj <alienchuj@gmail.com>
2023-03-09 14:24:06 +0800

    beta diversity code refactor

--- mpsepi/rules/mpse_diversity_beta.smk
+++ mpsepi/rules/mpse_diversity_beta.smk
@@ -1,60 +1,202 @@
-rule mpse_diversity_beta:
+rule mpse_diversity_beta_cal:
     input:
         os.path.join(config["output"]["diversity_alpha"], "mpse/mpse.rds")
     output:
         dist_tsv = os.path.join(config["output"]["diversity_beta"], "mpse/dist.tsv"),
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    params:
+        mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
+        group = config["params"]["group"],
+        method = config["params"]["import_from"],
+        distmethod = config["params"]["diversity_beta"]["distmethod"]
+    conda:
+        config["envs"]["mpse"]
+    shell:
+        '''
+        Rscript {params.mpse_diversity_beta} \
+        cal \
+        {params.method} \
+        {params.distmethod} \
+        {input} \
+        {params.group} \
+        {output.dist_tsv} \
+        {output.mpse}
+        '''
+
+
+rule mpse_diversity_beta_plot_dist:
+    input:
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    output:
         dist_samples_plot = expand(os.path.join(
             config["output"]["diversity_beta"], "plot/dist_samples.{outformat}"),
             outformat=["pdf", "svg", "png"]),
         dist_groups_plot = expand(os.path.join(
             config["output"]["diversity_beta"], "plot/dist_groups.{outformat}"),
-            outformat=["pdf", "svg", "png"]),
-        pca_plot = expand(os.path.join(
-            config["output"]["diversity_beta"], "plot/pca.{outformat}"),
-            outformat=["pdf", "svg", "png"]),
-        pcoa_plot = expand(os.path.join(
-            config["output"]["diversity_beta"], "plot/pcoa.{outformat}"),
-            outformat=["pdf", "svg", "png"]),
-        clust_plot = expand(os.path.join(
-            config["output"]["diversity_beta"], "plot/clust.{outformat}"),
-            outformat=["pdf", "svg", "png"]),
-        image = os.path.join(config["output"]["diversity_beta"], "mpse/diversity_beta.RData")
+            outformat=["pdf", "svg", "png"])
     params:
         mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
         group = config["params"]["group"],
         method = config["params"]["import_from"],
         distmethod = config["params"]["diversity_beta"]["distmethod"],
         plot_outdir = os.path.join(config["output"]["diversity_beta"], "plot/"),
-        h1 = config["params"]["diversity_beta"]["plot"]["dist_samples"]["height"],
-        w1 = config["params"]["diversity_beta"]["plot"]["dist_samples"]["width"],
-        h2 = config["params"]["diversity_beta"]["plot"]["dist_groups"]["height"],
-        w2 = config["params"]["diversity_beta"]["plot"]["dist_groups"]["width"],
-        h3 = config["params"]["diversity_beta"]["plot"]["pca"]["height"],
-        w3 = config["params"]["diversity_beta"]["plot"]["pca"]["width"],
-        h4 = config["params"]["diversity_beta"]["plot"]["pcoa"]["height"],
-        w4 = config["params"]["diversity_beta"]["plot"]["pcoa"]["width"],
-        h5 = config["params"]["diversity_beta"]["plot"]["clust"]["height"],
-        w5 = config["params"]["diversity_beta"]["plot"]["clust"]["width"]
+        height_samples = config["params"]["diversity_beta"]["plot"]["dist_samples"]["height"],
+        width_samples = config["params"]["diversity_beta"]["plot"]["dist_samples"]["width"],
+        height_groups = config["params"]["diversity_beta"]["plot"]["dist_groups"]["height"],
+        width_groups = config["params"]["diversity_beta"]["plot"]["dist_groups"]["width"]
     conda:
         config["envs"]["mpse"]
     shell:
         '''
-        Rscript {params.mpse_diversity_beta} {params.method} \
+        Rscript {params.mpse_diversity_beta} \
+        plot \
+        dist \
+        {params.method} \
         {params.distmethod} \
         {input} \
         {params.group} \
-        {output.dist_tsv} \
         {params.plot_outdir} \
-        {output.image} \
-        {params.h1} {params.w1} \
-        {params.h2} {params.w2} \
-        {params.h3} {params.w3} \
-        {params.h4} {params.w4} \
-        {params.h5} {params.w5}
+        {params.height_samples} \
+        {params.width_samples} \
+        {params.height_groups} \
+        {params.width_groups}
+        '''
+
+
+rule mpse_diversity_beta_plot_pca:
+    input:
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    output:
+        pca_plot = expand(os.path.join(
+            config["output"]["diversity_beta"], "plot/pca.{outformat}"),
+            outformat=["pdf", "svg", "png"])
+    params:
+        mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
+        group = config["params"]["group"],
+        method = config["params"]["import_from"],
+        distmethod = config["params"]["diversity_beta"]["distmethod"],
+        plot_outdir = os.path.join(config["output"]["diversity_beta"], "plot/"),
+        height = config["params"]["diversity_beta"]["plot"]["pca"]["height"],
+        width = config["params"]["diversity_beta"]["plot"]["pca"]["width"]
+    conda:
+        config["envs"]["mpse"]
+    shell:
+        '''
+        Rscript {params.mpse_diversity_beta} \
+        plot \
+        pca \
+        {params.method} \
+        {params.distmethod} \
+        {input} \
+        {params.group} \
+        {params.plot_outdir} \
+        {params.height} \
+        {params.width}
+        '''
+
+
+rule mpse_diversity_beta_plot_pcoa:
+    input:
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    output:
+        pcoa_plot = expand(os.path.join(
+            config["output"]["diversity_beta"], "plot/pcoa.{outformat}"),
+            outformat=["pdf", "svg", "png"])
+    params:
+        mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
+        group = config["params"]["group"],
+        method = config["params"]["import_from"],
+        distmethod = config["params"]["diversity_beta"]["distmethod"],
+        plot_outdir = os.path.join(config["output"]["diversity_beta"], "plot/"),
+        height = config["params"]["diversity_beta"]["plot"]["pcoa"]["height"],
+        width = config["params"]["diversity_beta"]["plot"]["pcoa"]["width"]
+    conda:
+        config["envs"]["mpse"]
+    shell:
+        '''
+        Rscript {params.mpse_diversity_beta} \
+        plot \
+        pcoa \
+        {params.method} \
+        {params.distmethod} \
+        {input} \
+        {params.group} \
+        {params.plot_outdir} \
+        {params.height} \
+        {params.width}
+        '''
+
+
+rule mpse_diversity_beta_plot_nmds:
+    input:
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    output:
+        nmds_plot = expand(os.path.join(
+            config["output"]["diversity_beta"], "plot/nmds.{outformat}"),
+            outformat=["pdf", "svg", "png"])
+    params:
+        mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
+        group = config["params"]["group"],
+        method = config["params"]["import_from"],
+        distmethod = config["params"]["diversity_beta"]["distmethod"],
+        plot_outdir = os.path.join(config["output"]["diversity_beta"], "plot/"),
+        height = config["params"]["diversity_beta"]["plot"]["nmds"]["height"],
+        width = config["params"]["diversity_beta"]["plot"]["nmds"]["width"]
+    conda:
+        config["envs"]["mpse"]
+    shell:
+        '''
+        Rscript {params.mpse_diversity_beta} \
+        plot \
+        nmds \
+        {params.method} \
+        {params.distmethod} \
+        {input} \
+        {params.group} \
+        {params.plot_outdir} \
+        {params.height} \
+        {params.width}
+        '''
+
+
+rule mpse_diversity_beta_plot_clust:
+    input:
+        mpse = os.path.join(config["output"]["diversity_beta"], "mpse/mpse.rds")
+    output:
+        clust_plot = expand(os.path.join(
+            config["output"]["diversity_beta"], "plot/clust.{outformat}"),
+            outformat=["pdf", "svg", "png"])
+    params:
+        mpse_diversity_beta = os.path.join(WRAPPERS_DIR, "mpse_diversity_beta.R"),
+        group = config["params"]["group"],
+        method = config["params"]["import_from"],
+        distmethod = config["params"]["diversity_beta"]["distmethod"],
+        plot_outdir = os.path.join(config["output"]["diversity_beta"], "plot/"),
+        height = config["params"]["diversity_beta"]["plot"]["clust"]["height"],
+        width = config["params"]["diversity_beta"]["plot"]["clust"]["width"]
+    conda:
+        config["envs"]["mpse"]
+    shell:
+        '''
+        Rscript {params.mpse_diversity_beta} \
+        plot \
+        clust \
+        {params.method} \
+        {params.distmethod} \
+        {input} \
+        {params.group} \
+        {params.plot_outdir} \
+        {params.height} \
+        {params.width}
         '''
 
 
 rule mpse_diversity_beta_all:
     input:
-        rules.mpse_diversity_beta.output
+        rules.mpse_diversity_beta_cal.output,
+        rules.mpse_diversity_beta_plot_dist.output,
+        rules.mpse_diversity_beta_plot_pca.output,
+        rules.mpse_diversity_beta_plot_pcoa.output,
+        rules.mpse_diversity_beta_plot_nmds.output,
+        rules.mpse_diversity_beta_plot_clust.output
  
--- mpsepi/wrappers/mpse_diversity_beta.R
+++ mpsepi/wrappers/mpse_diversity_beta.R
@@ -3,9 +3,15 @@
 'mpse diversity beta script
 
 Usage:
-  mpse_diversity_beta.R <method> <distmethod> <mpse> <group> <dist_tsv> <plot_outdir> <image> <h1> <w1> <h2> <w2> <h3> <w3> <h4> <w4> <h5> <w5>
-  mpse_import.R (-h | --help)
-  mpse_import.R --version
+  mpse_diversity_beta.R cal <method> <distmethod> <mpse> <group> <dist_tsv> <mpse_output>
+  mpse_diversity_beta.R plot dist <method> <distmethod> <mpse> <group> <plot_outdir> <h_samples> <w_samples> <h_groups> <w_groups>
+  mpse_diversity_beta.R plot pca <method> <distmethod> <mpse> <group> <plot_outdir> <height> <width>
+  mpse_diversity_beta.R plot pcoa <method> <distmethod> <mpse> <group> <plot_outdir> <height> <width>
+  mpse_diversity_beta.R plot nmds <method> <distmethod> <mpse> <group> <plot_outdir> <height> <width>
+  mpse_diversity_beta.R plot clust <method> <distmethod> <mpse> <group> <plot_outdir> <height> <width>
+
+  mpse_diversity_beta.R (-h | --help)
+  mpse_diversity_beta.R --version
 
 Options:
   -h --help     Show this screen.
@@ -24,242 +30,305 @@ library(ggtreeExtra)
 args <- docopt::docopt(doc, version = 'mpse diversity beta v0.1')
 
 
-if (!dir.exists(args$plot_outdir)) {
-  dir.create(args$plot_outdir, recursive = TRUE)
+if (args$cal) {
+  mpse <- readRDS(args$mpse)
+
+  mpse %<>% 
+    MicrobiotaProcess::mp_decostand(
+      .abundance = Abundance)
+
+  # cal dist
+  mpse %<>%
+    MicrobiotaProcess::mp_cal_dist(
+      .abundance = hellinger,
+      distmethod = args$distmethod)
+
+  # extract distance
+  mpse_dist <- as.matrix(mpse %>% MicrobiotaProcess::mp_extract_dist(distmethod = args$distmethod))
+  mpse_dist[lower.tri(mpse_dist, diag=TRUE)] <- NA
+  #print(mpse_dist)
+
+  mpse_dist <-
+    mpse_dist %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("Sample_a") %>%
+    tidyr::pivot_longer(
+      -c(Sample_a),
+      names_to = "Sample_b",
+      values_to = "distance") %>%
+    dplyr::filter(!is.na(distance))
+
+  if (!dir.exists(dirname(args$dist_tsv))) {
+    dir.create(dirname(args$dist_tsv), recursive = TRUE)
+  }
+  readr::write_tsv(mpse_dist, args$dist_tsv)
+
+  # cal pca
+  mpse %<>% 
+    MicrobiotaProcess::mp_cal_pca(
+      .abundance = hellinger,
+      action = "add")
+
+  # cal pcoa
+  mpse %<>% 
+    MicrobiotaProcess::mp_cal_pcoa(
+      .abundance = hellinger,
+      distmethod = args$distmethod,
+      action = "add")
+
+
+  # cal nmds
+  mpse %<>% 
+    MicrobiotaProcess::mp_cal_nmds(
+      .abundance = hellinger,
+      distmethod = args$distmethod,
+      action = "add",
+      seed = 2023)
+
+  # clust
+  mpse %<>%
+    MicrobiotaProcess::mp_cal_clust(
+      .abundance = hellinger, 
+      distmethod = args$distmethod,
+      hclustmethod = "average",
+      action = "add"
+  )
+
+  if (!dir.exists(dirname(args$mpse_output))) {
+    dir.create(dirname(args$mpse_output), recursive = TRUE)
+  }
+  saveRDS(mpse, args$mpse_output)
 }
 
-mpse <- readRDS(args$mpse)
 
-mpse %<>% 
-  MicrobiotaProcess::mp_decostand(
-    .abundance = Abundance)
+if (args$plot) {
+  mpse <- readRDS(args$mpse)
 
+  if (!dir.exists(args$plot_outdir)) {
+    dir.create(args$plot_outdir, recursive = TRUE)
+  }
 
-# cal dist
-mpse %<>%
-  MicrobiotaProcess::mp_cal_dist(
-    .abundance = hellinger,
-    distmethod = args$distmethod)
+  if (args$dist) {
+    # plot dist
+    # samples distance
+    p1 <- mpse %>%
+      MicrobiotaProcess::mp_plot_dist(
+        .distmethod = !!rlang::sym(args$distmethod),
+        .group = !!rlang::sym(args$group))
 
+    # groups distance
+    p2 <- mpse %>%
+      MicrobiotaProcess::mp_plot_dist(
+        .distmethod = !!rlang::sym(args$distmethod),
+        .group = !!rlang::sym(args$group),
+        group.test = TRUE,
+        textsize = 2)
 
-# extract distance
-mpse_dist <- as.matrix(mpse %>% MicrobiotaProcess::mp_extract_dist(distmethod = args$distmethod))
-mpse_dist[lower.tri(mpse_dist, diag=TRUE)] <- NA
-#print(mpse_dist)
+    h1 <- as.numeric(args$h_samples)
+    w1 <- as.numeric(args$w_samples)
+    h2 <- as.numeric(args$h_groups)
+    w2 <- as.numeric(args$h_groups)
+  
+    ggsave(stringr::str_c(args$plot_outdir, "dist_samples.pdf"), p1, 
+      height=h1, width=w1, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "dist_samples.svg"), p1,
+      height=h1, width=w1, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "dist_samples.png"), p1,
+      height=h1, width=w1, limitsize = FALSE)
 
-mpse_dist <-
-  mpse_dist %>%
-  as.data.frame() %>%
-  tibble::rownames_to_column("Sample_a") %>%
-  tidyr::pivot_longer(
-    -c(Sample_a),
-    names_to = "Sample_b",
-    values_to = "distance") %>%
-  dplyr::filter(!is.na(distance))
+    ggsave(stringr::str_c(args$plot_outdir, "dist_groups.pdf"), p2,
+      height=h2, width=w2, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "dist_groups.svg"), p2,
+      height=h2, width=w2, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "dist_groups.png"), p2,
+      height=h2, width=w2, limitsize = FALSE)
+  }
 
-if (!dir.exists(dirname(args$dist_tsv))) {
-  dir.create(dirname(args$dist_tsv), recursive = TRUE)
-}
-readr::write_tsv(mpse_dist, args$dist_tsv)
-
-
-# plot dist
-# samples distance
-p1 <- mpse %>%
-  MicrobiotaProcess::mp_plot_dist(
-    .distmethod = !!rlang::sym(args$distmethod),
-    .group = !!rlang::sym(args$group))
-
-# groups distance
-p2 <- mpse %>%
-  MicrobiotaProcess::mp_plot_dist(
-    .distmethod = !!rlang::sym(args$distmethod),
-    .group = !!rlang::sym(args$group),
-    group.test = TRUE,
-    textsize = 2)
-
-
-# cal pca
-mpse %<>% 
-  MicrobiotaProcess::mp_cal_pca(
-    .abundance = hellinger, action = "add")
-
-
-# plot pca
-pca_p1 <- mpse %>%
-  MicrobiotaProcess::mp_plot_ord(
-    .ord = pca, 
-    .group = !!rlang::sym(args$group), 
-    .color = !!rlang::sym(args$group),
-    #.starshape = !!rlang::sym(args$group),
-    .size = 1.2,
-    .alpha = 1,
-    ellipse = TRUE,
-    show.legend = FALSE
-)
-
-pca_p2 <- mpse %>%
-  MicrobiotaProcess::mp_plot_ord(
-    .ord = pca, 
-    .group = !!rlang::sym(args$group), 
-    .color = !!rlang::sym(args$group), 
-    #.starshape = !!rlang::sym(args$group),
-    .size = Observe, 
-    .alpha = Shannon,
-    ellipse = TRUE,
-    show.legend = FALSE
-)
-
-pca_p <- pca_p1 + pca_p2
-
-
-# cal pcoa
-mpse %<>% 
-  MicrobiotaProcess::mp_cal_pcoa(
-    .abundance = hellinger, distmethod = args$distmethod)
-
-
-# plot pcoa
-pcoa_p1 <- mpse %>%
-  MicrobiotaProcess::mp_plot_ord(
-    .ord = pcoa, 
-    .group = !!rlang::sym(args$group), 
-    .color = !!rlang::sym(args$group), 
-    #.starshape = !!rlang::sym(args$group),
-    .size = 1.2,
-    .alpha = 1,
-    ellipse = TRUE,
-    show.legend = FALSE
-)
-
-pcoa_p2 <- mpse %>%
-  MicrobiotaProcess::mp_plot_ord(
-    .ord = pcoa, 
-    .group = !!rlang::sym(args$group), 
-    .color = !!rlang::sym(args$group), 
-    #.starshape = !!rlang::sym(args$group),
-    .size = Observe, 
-    .alpha = Shannon,
-    ellipse = TRUE,
-    show.legend = FALSE
-)
-
-pcoa_p <- pcoa_p1 + pcoa_p2
-
-
-# clust
-mpse %<>%
-  MicrobiotaProcess::mp_cal_clust(
-    .abundance = hellinger, 
-    distmethod = args$distmethod,
-    hclustmethod = "average",
-    action = "add"
-)
-
-samples_clust <- mpse %>% MicrobiotaProcess::mp_extract_internal_attr(name='SampleClust')
-
-f <- ggtree(samples_clust) + 
-  geom_tippoint(aes(color = !!rlang::sym(args$group))) +
-  geom_tiplab(as_ylab = TRUE) +
-  ggplot2::scale_x_continuous(expand = c(0, 0.01))
-
-phyla_tb <- mpse %>% MicrobiotaProcess::mp_extract_abundance(taxa.class = Phylum, topn = 30)
-
-if (args$method %in% c("dada2", "qiime2")) {
-
-  phyla_tb %<>% tidyr::unnest(cols = RareAbundanceBySample) %>% dplyr::rename(Phyla = "label")
-
-  f <- f +
-    geom_fruit(
-      data = phyla_tb,
-      geom = geom_col,
-      mapping = aes(x = RelRareAbundanceBySample, 
-                    y = Sample, 
-                    fill = Phyla),
-      orientation = "y",
-      #offset = 0.4,
-      pwidth = 3, 
-      axis.params = list(axis = "x", 
-                         title = "The relative abundance of phyla (%)",
-                         title.size = 4,
-                         text.size = 2, 
-                         vjust = 1),
-      grid.params = list()
+  if (args$pca) {
+    # plot pca
+    pca_p1 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = pca, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group),
+        #.starshape = !!rlang::sym(args$group),
+        .size = 1.2,
+        .alpha = 1,
+        ellipse = TRUE,
+        show.legend = FALSE
     )
-
-} else if (args$method == "metaphlan") {
-
-  phyla_tb %<>% tidyr::unnest(cols = AbundanceBySample) %>% dplyr::rename(Phyla = "label")
-
-  f <- f +
-    geom_fruit(
-      data = phyla_tb,
-      geom = geom_col,
-      mapping = aes(x = RelAbundanceBySample, 
-                    y = Sample, 
-                    fill = Phyla),
-      orientation = "y",
-      #offset = 0.4,
-      pwidth = 3, 
-      axis.params = list(axis = "x", 
-                         title = "The relative abundance of phyla (%)",
-                         title.size = 4,
-                         text.size = 2, 
-                         vjust = 1),
-      grid.params = list()
+    pca_p2 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = pca, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group), 
+        #.starshape = !!rlang::sym(args$group),
+        .size = Observe, 
+        .alpha = Shannon,
+        ellipse = TRUE,
+        show.legend = FALSE
     )
+    pca_p <- pca_p1 + pca_p2
+
+    h <- as.numeric(args$height)
+    w <- as.numeric(args$width)
+
+    ggsave(stringr::str_c(args$plot_outdir, "pca.pdf"), pca_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "pca.svg"), pca_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "pca.png"), pca_p,
+      height=h, width=w, limitsize = FALSE)
+  }
+
+  if (args$pcoa) {
+    # plot pcoa
+    pcoa_p1 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = pcoa, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group), 
+        .starshape = !!rlang::sym(args$group),
+        .size = 1.2,
+        .alpha = 1,
+        ellipse = TRUE,
+        show.legend = FALSE
+    )
+    pcoa_p2 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = pcoa, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group), 
+        .starshape = !!rlang::sym(args$group),
+        .size = Observe, 
+        .alpha = Shannon,
+        ellipse = TRUE,
+        show.legend = FALSE
+    )
+    pcoa_p <- pcoa_p1 + pcoa_p2
+
+    h <- as.numeric(args$height)
+    w <- as.numeric(args$width)
+
+    ggsave(stringr::str_c(args$plot_outdir, "pcoa.pdf"), pcoa_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "pcoa.svg"), pcoa_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "pcoa.png"), pcoa_p,
+      height=h, width=w, limitsize = FALSE)
+  }
+
+  if (args$nmds) {
+    # plot nmds
+    nmds_p1 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = nmds, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group), 
+        .starshape = !!rlang::sym(args$group),
+        .size = 1.2,
+        .alpha = 1,
+        ellipse = TRUE,
+        show.legend = FALSE
+    )
+    nmds_p2 <- mpse %>%
+      MicrobiotaProcess::mp_plot_ord(
+        .ord = nmds, 
+        .group = !!rlang::sym(args$group), 
+        .color = !!rlang::sym(args$group), 
+        .starshape = !!rlang::sym(args$group),
+        .size = Observe, 
+        .alpha = Shannon,
+        ellipse = TRUE,
+        show.legend = FALSE
+    )
+    nmds_p <- nmds_p1 + nmds_p2
+
+    h <- as.numeric(args$height)
+    w <- as.numeric(args$width)
+
+    ggsave(stringr::str_c(args$plot_outdir, "nmds.pdf"), nmds_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "nmds.svg"), nmds_p,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "nmds.png"), nmds_p,
+      height=h, width=w, limitsize = FALSE)
+  }
+
+  if (args$clust) {
+    samples_clust <-
+      mpse %>%
+      MicrobiotaProcess::mp_extract_internal_attr(name='SampleClust')
+
+    f <- ggtree(samples_clust) + 
+      geom_tippoint(aes(color = !!rlang::sym(args$group))) +
+      geom_tiplab(as_ylab = TRUE) +
+      ggplot2::scale_x_continuous(expand = c(0, 0.01))
+
+    phyla_tb <-
+      mpse %>%
+      MicrobiotaProcess::mp_extract_abundance(taxa.class = Phylum, topn = 30)
+
+    if (args$method %in% c("dada2", "qiime2")) {
+      phyla_tb %<>%
+        tidyr::unnest(cols = RareAbundanceBySample) %>%
+        dplyr::rename(Phyla = "label")
+
+      f <- f +
+        geom_fruit(
+          data = phyla_tb,
+          geom = geom_col,
+          mapping = aes(
+            x = RelRareAbundanceBySample, 
+            y = Sample, 
+            fill = Phyla),
+          orientation = "y",
+          #offset = 0.4,
+          pwidth = 3, 
+          axis.params = list(
+            axis = "x", 
+            title = "The relative abundance of phyla (%)",
+            title.size = 4,
+            text.size = 2, 
+            vjust = 1),
+          grid.params = list()
+        )
+    } else if (args$method == "metaphlan") {
+      phyla_tb %<>%
+        tidyr::unnest(cols = AbundanceBySample) %>%
+        dplyr::rename(Phyla = "label")
+
+      f <- f +
+        geom_fruit(
+          data = phyla_tb,
+          geom = geom_col,
+          mapping = aes(
+            x = RelAbundanceBySample, 
+            y = Sample, 
+            fill = Phyla
+          ),
+          orientation = "y",
+          #offset = 0.4,
+          pwidth = 3, 
+          axis.params = list(
+            axis = "x", 
+            title = "The relative abundance of phyla (%)",
+            title.size = 4,
+            text.size = 2, 
+            vjust = 1
+          ),
+          grid.params = list()
+        )
+    }
+
+    h <- as.numeric(args$height)
+    w <- as.numeric(args$width)
+
+    ggsave(stringr::str_c(args$plot_outdir, "clust.pdf"), f,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "clust.svg"), f,
+      height=h, width=w, limitsize = FALSE)
+    ggsave(stringr::str_c(args$plot_outdir, "clust.png"), f,
+      height=h, width=w, limitsize = FALSE)
+  }
 }
-
-
-# save plot
-h1 <- as.numeric(args$h1)
-w1 <- as.numeric(args$w1)
-h2 <- as.numeric(args$h2)
-w2 <- as.numeric(args$w2)
-h3 <- as.numeric(args$h3)
-w3 <- as.numeric(args$w3)
-h4 <- as.numeric(args$h4)
-w4 <- as.numeric(args$w4)
-h5 <- as.numeric(args$h5)
-w5 <- as.numeric(args$w5)
-
-ggsave(stringr::str_c(args$plot_outdir, "dist_samples.pdf"), p1, 
-  height=h1, width=w1, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "dist_samples.svg"), p1,
-  height=h1, width=w1, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "dist_samples.png"), p1,
-  height=h1, width=w1, limitsize = FALSE)
-
-ggsave(stringr::str_c(args$plot_outdir, "dist_groups.pdf"), p2,
-  height=h2, width=w2, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "dist_groups.svg"), p2,
-  height=h2, width=w2, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "dist_groups.png"), p2,
-  height=h2, width=w2, limitsize = FALSE)
-
-ggsave(stringr::str_c(args$plot_outdir, "pca.pdf"), pca_p,
-  height=h3, width=w3, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "pca.svg"), pca_p,
-  height=h3, width=w3, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "pca.png"), pca_p,
-  height=h3, width=w3, limitsize = FALSE)
-
-ggsave(stringr::str_c(args$plot_outdir, "pcoa.pdf"), pcoa_p,
-  height=h4, width=w4, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "pcoa.svg"), pcoa_p,
-  height=h4, width=w4, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "pcoa.png"), pcoa_p,
-  height=h4, width=w4, limitsize = FALSE)
-
-ggsave(stringr::str_c(args$plot_outdir, "clust.pdf"), f,
-  height=h5, width=w5, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "clust.svg"), f,
-  height=h5, width=w5, limitsize = FALSE)
-ggsave(stringr::str_c(args$plot_outdir, "clust.png"), f,
-  height=h5, width=w5, limitsize = FALSE)
-
-
-if (!dir.exists(dirname(args$image))) {
-  dir.create(dirname(args$image), recursive = TRUE)
-}
-save.image(args$image)
