22e760d53ec8bc0df7d7514c42d1ff64fc778c3e
alienzj <alienchuj@gmail.com>
2023-03-22 18:54:41 +0800

    functions import works

--- mpsepi/wrappers/mpse_function.R
+++ mpsepi/wrappers/mpse_function.R
@@ -39,18 +39,13 @@ args <- docopt::docopt(doc, version = 'mpse function v0.1')
 
 if (args$import) {
 
-  metadata_df <-
-    readr::read_tsv(args$metafile) %>%
-    dplyr::rename("Sample" = "sample_name")
+  metadata_df <- readr::read_tsv(args$metafile)
 
   func_df <- readr::read_tsv(args$funcfile)
   func_df %<>% tibble::column_to_rownames(colnames(func_df)[1])
 
   mpse <- MicrobiotaProcess::MPSE(assays = list(Abundance = func_df))
-  print(mpse)
-
-  mpse %<>% dplyr::left_join(metadata_df)
-
+  mpse %<>% dplyr::left_join(metadata_df, by=c("Sample" = "sample_name"))
 
   if (!dir.exists(dirname(args$mpse_output))) {
     dir.create(dirname(args$mpse_output), recursive = TRUE)
